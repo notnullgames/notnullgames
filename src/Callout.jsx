00920f1ecc10cd252ac1e5f7ad71b46407e3ef3b
David Konsumer <konsumer@jetboystudio.com>
2025-06-23 04:09:04 -0700

    tweaking content

--- src/Callout.jsx
+++ src/Callout.jsx
@@ -1,6 +1,6 @@
-export default function Callout({ emoji, type, children }) {
+export default function Callout({ emoji, className, children }) {
   return (
-    <div role='alert' className={`alert text-xl p-4${type ? ` alert-${type}` : ''}`}>
+    <div role='alert' className={`alert text-xl p-4 ${className}`}>
       {emoji && <span className='text-4xl'>{emoji}</span>}
       <span>{children}</span>
     </div>
