50aaaa83bea25c96437e4aea8aa1bddb1aa77cdd
David Konsumer <konsumer@jetboystudio.com>
2025-06-26 15:21:33 -0700

    nicer js demo

--- examples/js/example/main.js
+++ examples/js/example/main.js
@@ -7,9 +7,13 @@ export function load() {
 }
 
 export function update() {
-  clear(BLUE)
+  clear(BLACK)
   draw_rectangle(10, 10, 100, 50, RED)
+  draw_rectangle(100, 50, 100, 50, YELLOW)
+  draw_rectangle(200, 100, 100, 50, BLUE)
+  draw_rectangle(300, 150, 200, 50, GREEN)
+  draw_circle(400, 300, 50, LIME)
   if (key_down(KEY_SPACE)) {
-    draw_text(FONT_DEFAULT, 'Space pressed!', 50, 50, WHITE)
+    draw_text(FONT_DEFAULT, 'Space pressed!', 350, 300, WHITE)
   }
 }
